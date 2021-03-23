c0abd8f303ccf500340e88d7d0e76abd172de49f
LAPTOP-KDFURSCG\temka <temkakb@gmail.com>
2021-03-23 15:59:55 +0700

    sua attribute username thanh userName cho dong bo, them POST, them url login vao config permitall

--- src/main/java/com/example/Quiz/API/UserAPI.java
+++ src/main/java/com/example/Quiz/API/UserAPI.java
@@ -3,6 +3,7 @@ package com.example.Quiz.API;
 import com.example.Quiz.Models.User;
 import com.example.Quiz.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 
 import java.security.Principal;
+import java.util.Map;
 
 @RestController
 
@@ -20,13 +22,25 @@ public class UserAPI {
     @Autowired
     UserRepository userRepository;
     @RequestMapping(value="/user", method = {RequestMethod.GET,RequestMethod.DELETE,RequestMethod.POST,RequestMethod.PUT})
-    public Object UserAPI_controler_vippro (Principal principal , HttpServletRequest httpServletRequest)
+    public Object UserAPI_controler_vippro (Principal principal , HttpServletRequest httpServletRequest, @RequestBody User user)
     {
 
         String method = httpServletRequest.getMethod();
         if (method.equals("GET"))
         {
             return  userService.Getuser(principal.getName());
+        }
+        if (method.equals("POST"))
+        {
+                return userService.PostUser(user);
+        }
+        if (method.equals("DELETE"))
+        {
+
+        }
+        if (method.equals("PUT"))
+        {
+
         }
         return  null;
 
